14a38c3b8ee84e318b36d591e35c966ef8994c7b
Usman Ali <usmanali.cs2044@gmail.com>
2025-12-31 20:12:11 +0500

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://car-marketplace-app-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
     credentials: true
